8bf366a1985ea1b7174bb1bc6901b904c1cd03f5
Lisias T <me@lisias.net>
2018-10-12 23:11:29 -0300

    Updating automation scripts

--- deploy.sh
+++ deploy.sh
@@ -10,17 +10,17 @@ check() {
 }
 
 deploy() {
-	local DLL=$1.dll
+	local DLL=$1
 
-	if [ -f "./bin/Release/$DLL" ] ; then
-		cp "./bin/Release/$DLL" "./GameData/$TARGETBINDIR/"
+	if [ -f "./bin/Release/$DLL.dll" ] ; then
+		cp "./bin/Release/$DLL.dll" "./GameData/$TARGETBINDIR/"
 		if [ -d "${KSP_DEV}/GameData/$TARGETBINDIR/" ] ; then
-			cp "./bin/Release/$DLL" "${KSP_DEV/}GameData/$TARGETBINDIR/"
+			cp "./bin/Release/$DLL.dll" "${KSP_DEV/}GameData/$TARGETBINDIR/"
 		fi
 	fi
-	if [ -f "./bin/Debug/$DLL" ] ; then
+	if [ -f "./bin/Debug/$DLL.dll" ] ; then
 		if [ -d "${KSP_DEV}/GameData/$TARGETBINDIR/" ] ; then
-			cp "./bin/Debug/$DLL" "${KSP_DEV}GameData/$TARGETBINDIR/"
+			cp "./bin/Debug/$DLL.dll" "${KSP_DEV}GameData/$TARGETBINDIR/"
 		fi
 	fi
 }
@@ -32,4 +32,7 @@ cp $VERSIONFILE "./GameData/$TARGETDIR"
 cp CHANGE_LOG.md "./GameData/$TARGETDIR"
 cp README.md  "./GameData/$TARGETDIR"
 cp LICENSE "./GameData/$TARGETDIR"
-deploy $PACKAGE
+cp NOTICE "./GameData/$TARGETDIR"
+for dll in $PACKAGE ; do
+    deploy $dll
+done
